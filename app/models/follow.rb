3cb73084f21f59a7b203c923c0b0b2635582728e
Boston Essary <bostoncessary@gmail.com>
2024-12-23 23:13:43 -0700

    add test for uniq follows

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,5 +2,5 @@ class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followee, class_name: "User"
 
-  validates :follower, uniqeuess: { scope: :followee, message: "You can't follow the same person twice." }
+  validates :follower, uniqueness: { scope: :followee, message: "You can't follow the same person twice." }
 end
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Follow, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:user1) { create(:user) }
+
+  describe 'validations' do
+    context 'uniqueness validations' do
+      let!(:follow) { create(:follow, follower: user, followee: user1) }
+      let(:follow1) { build(:follow, follower: user, followee: user1) }
+
+      it ' should be invalid if another follow exists for this follower and followee' do
+        expect(follow1).to be_invalid
+      end
+    end
+  end
 end
