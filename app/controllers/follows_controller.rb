d4e405b30175b08ebf7e625cd28ef47fee3bdc63
Boston Essary <bostoncessary@gmail.com>
2024-12-25 13:44:37 -0700

    set before action on destroy and finish bad path for destroy spec

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,5 +1,6 @@
 class FollowsController < ApplicationController
   before_action :set_user, only: :create
+  before_action :set_follow, only: :destroy
 
   def create
     @follow = Follow.new(follower_id: current_user.id, followee_id: params[:user_id])
@@ -12,7 +13,6 @@ class FollowsController < ApplicationController
   end
 
   def destroy
-    @follow = Follow.find(params[:id])
     @user = @follow.followee
     if @follow.destroy
       redirect_to user_path(@user), notice: "You unfollowed #{ @user.username}"
@@ -28,6 +28,15 @@ class FollowsController < ApplicationController
 
     if @user.nil?
       flash[:alert] = "User not found"
+      render "dashboard/index", status: :unprocessable_entity
+    end
+  end
+
+  def set_follow
+    @follow = Follow.find_by(id: params[:id])
+
+    if @follow.nil?
+      flash[:alert] = "Follow could not be found"
       render "dashboard/index", status: :unprocessable_entity
     end
   end
