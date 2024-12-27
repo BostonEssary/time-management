2ccd942b5a68e6c3cca1a62036f1b82ac2a92c32
Boston Essary <bostoncessary@gmail.com>
2024-12-27 00:43:05 -0700

    add flash messages and a stimulus controller to clear them

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,18 @@
 module ApplicationHelper
   def flash_symbol(type)
-    symbol = '<i class="fa-solid fa-check"></i>'
+    if type.to_s == "notice"
+      symbol = '<i class="fa-solid fa-check"></i>'
+    else
+      symbol = '<i class="fa-solid fa-exclamation"></i>'
+    end
     symbol.html_safe
+  end
+
+  def flash_classes(type)
+    if type.to_s == "notice"
+      "bg-primary-200 border-2 border-primary-400 text-primary-800"
+    else
+      "bg-yellow-100 border-2 border-yellow-400 text-yellow-800"
+    end
   end
 end
---
+++ app/javascript/controllers/alerts_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="alerts"
+export default class extends Controller {
+  static targets = [ 'alert']
+  connect() {
+    console.log('alerts controller connected')
+  }
+
+  alertTargetConnected() {
+    setTimeout(() => {
+     this.alertTarget.remove() 
+    }, 3000);
+  }
+
+  dismiss(event) {
+    event.target
+    this.alertTarget.remove()
+    console.log('removed')
+  }
+}
