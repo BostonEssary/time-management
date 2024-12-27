dbed552ddc35cbed52d9259cfd9be204b6d898a4
Boston Essary <bostoncessary@gmail.com>
2024-12-27 00:44:35 -0700

    remove logs

--- app/javascript/controllers/alerts_controller.js
+++ app/javascript/controllers/alerts_controller.js
@@ -4,7 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ 'alert']
   connect() {
-    console.log('alerts controller connected')
   }
 
   alertTargetConnected() {
@@ -16,6 +15,5 @@ export default class extends Controller {
   dismiss(event) {
     event.target
     this.alertTarget.remove()
-    console.log('removed')
   }
 }
