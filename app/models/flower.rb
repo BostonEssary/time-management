df46c5ecb20dc1f6e7e7dfe092c5cc9800b2c67a
Boston Essary <bostoncessary@gmail.com>
2025-01-11 13:31:00 -0700

    add ability to upload an avatar

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -2,6 +2,8 @@ class Flower < ApplicationRecord
   STRAINS = [ "sativa", "indica", "hybrid" ].freeze
 
   belongs_to :brand
+  has_one_attached :avatar
+  has_many_attached :images
 
   validates :name, presence: true
   validates :name, uniqueness: { scope: :brand, message: "A product with that name already exists for that brand" }
