442a3521a1074a0079ddb99d46f8a1f5f05d2873
Boston Essary <bostoncessary@gmail.com>
2025-02-24 20:53:14 -0700

    show ratings on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
-  def index
+  def show
+    @ratings = Rating.includes(:user, :ratable, :likes, image_attachment: :blob)
+                    .where(user: current_user.followees)
+                    .order(created_at: :desc)
+                    .page(params[:page])
+                    .per(5)
   end
 end
