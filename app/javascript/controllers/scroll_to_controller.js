583cf9ec6d68b7e16ee1673e6ed0cbecdbcb5328
Boston Essary <bostoncessary@gmail.com>
2025-02-27 19:30:58 -0700

    remove comment

--- app/javascript/controllers/scroll_to_controller.js
+++ app/javascript/controllers/scroll_to_controller.js
@@ -3,10 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="scroll-to"
 export default class extends Controller {
   connect() {
-    console.log('scroll-to connected')
-
-    
-
-    this.element.scrollIntoView({ behavior: 'smooth' })
+    this.element.scrollIntoView({ behavior: 'smooth' }) 
   }
 }
