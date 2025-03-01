5e34c91a51c2fd073027ed093740546d1ee29eb0
Boston Essary <bostoncessary@gmail.com>
2025-02-28 22:07:34 -0700

    fix strain popovers

--- app/models/concerns/cannabis_product.rb
+++ app/models/concerns/cannabis_product.rb
@@ -4,25 +4,23 @@ module CannabisProduct
   included do
     STRAINS = [ "sativa", "indica", "hybrid" ].freeze
     STRAIN_EFFECTS = {
-      "sativa" => <<~DESC.squish,
-        Sativa strains are known for their energizing, uplifting effects. They typically promote#{' '}
-        creativity, focus, and mental clarity, making them popular for daytime use. Users often#{' '}
-        experience increased energy and enhanced mood, perfect for social activities, creative#{' '}
-        projects, or staying productive throughout the day.
-      DESC
+      "sativa" =>
+        "Sativa strains are known for their energizing, uplifting effects. They typically promote
+        creativity, focus, and mental clarity, making them popular for daytime use. Users often
+        experience increased energy and enhanced mood, perfect for social activities, creative
+        projects, or staying productive throughout the day.",
 
-      "indica" => <<~DESC.squish,
-        Indica strains are known for their relaxing, sedative effects. They are often used to#{' '}
-        treat pain, inflammation, and insomnia. Users often experience a sense of calm and#{' '}
-        relaxation, perfect for unwinding after a long day or before bedtime.
-      DESC
 
-      "hybrid" => <<~DESC.squish
-        Hybrid strains are a combination of sativa and indica effects. They are known for their#{' '}
-        balanced effects, making them popular for a wide range of activities. Users often experience#{' '}
-        a sense of balance and relaxation, perfect for social activities, creative projects, or#{' '}
-        staying productive throughout the day.
-      DESC
+      "indica" =>
+        "Indica strains are known for their relaxing, sedative effects. They are often used to
+        treat pain, inflammation, and insomnia. Users often experience a sense of calm and
+        relaxation, perfect for unwinding after a long day or before bedtime.",
+
+      "hybrid" =>
+        "Hybrid strains are a combination of sativa and indica effects. They are known for their
+        balanced effects, making them popular for a wide range of activities. Users often experience
+        a sense of balance and relaxation, perfect for social activities, creative projects, or
+        staying productive throughout the day."
     }.freeze
 
     belongs_to :brand
